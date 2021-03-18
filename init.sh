2d3366f35898184118bf08f0dfb6b8da70472ed6
ahmad saufwan sazali <asfwan@gmail.com>
2021-03-18 11:13:20 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 sudo apt-get update
+
+sudo apt -y install software-properties-common
+sudo add-apt-repository ppa:ondrej/php
+
 sudo apt-get install -y php php-gd php-mbstring php-xml php-mysql php-curl
 sudo apt-get install -y composer
