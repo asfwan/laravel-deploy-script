b757bb3ae9ed26cb29a5628ebf942f3be2d29ef5
ahmad saufwan sazali <asfwan@gmail.com>
2021-03-18 11:59:09 +0800

    updated composer installer

--- init.sh
+++ init.sh
@@ -6,4 +6,8 @@ sudo apt -y install software-properties-common
 sudo add-apt-repository ppa:ondrej/php
 
 sudo apt-get install -y php php-gd php-mbstring php-xml php-mysql php-curl php-zip
-sudo apt-get install -y composer
+
+php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+php -r "if (hash_file('sha384', 'composer-setup.php') === '756890a4488ce9024fc62c56153228907f1545c228516cbf63f885e036d37e9a59d27d63f46af1d4d07ee0f76181c7d3') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+php composer-setup.php --install-dir=bin --filename=composer
+php -r "unlink('composer-setup.php');"
