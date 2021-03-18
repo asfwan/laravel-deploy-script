a71a63385255f226522857d52109b47bebb33387
ahmad saufwan sazali <asfwan@gmail.com>
2021-03-18 09:46:46 +0800

    Update init.sh

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install php php-gd php-mbstring php-xml php-mysql php-curl
-sudo apt-get install composer
+sudo apt-get install -y php php-gd php-mbstring php-xml php-mysql php-curl
+sudo apt-get install -y composer
